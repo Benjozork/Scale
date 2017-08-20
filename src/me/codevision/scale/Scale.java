7c1ebc8ecae16d7ad180244baae25fe7c2978c07
Benjozork <benjozorkfr@gmail.com>
2017-08-20 02:16:03 -0400

    better than nothing

---
+++ src/me/codevision/scale/Scale.java
@@ -0,0 +1,18 @@
+package me.codevision.scale;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Benjozork
+ */
+public class Scale extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
